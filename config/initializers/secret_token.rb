732a6e30176cafa29293034061f9afa068c4096d
robinhwang <binbinhuangcsu@gmail.com>
2013-06-01 22:09:41 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PosHomework::Application.config.secret_token = '89ac77a7c854b0d0221cf3d3750555f5557dc44c709fd6e12ee091a4b7c90b2af561ab1a5646b913fdf27d2ed7eb2db223f06a3eb1ddee9d10b8149546afce0d'
