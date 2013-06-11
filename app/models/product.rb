b084d49e6eb58f20e23e5e5b0540a3ee07004bcd
robinhwang <binbinhuangcsu@gmail.com>
2013-06-11 14:56:50 +0800

    have the homepage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :title, :price, :intro
-  validates :title, :price, :intor, prensence: true
+  validates :title, :price, :intor, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
 
