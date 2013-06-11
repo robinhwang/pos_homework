613500bbc17729a27df07537db2d3488cdf35dda
robinhwang <binbinhuangcsu@gmail.com>
2013-06-11 14:53:51 +0800

    validate data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :title, :price, :intro
+  validates :title, :price, :intor, prensence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+
   has_many :line_items
 end
