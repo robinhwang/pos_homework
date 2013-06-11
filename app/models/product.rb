dce19e08b84f654d00e3bed37573edbc1e02717d
robinhwang <binbinhuangcsu@gmail.com>
2013-06-11 15:14:38 +0800

    more about this cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,14 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
 
   has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line items present')
+      return false
+    end
+  end
 end
